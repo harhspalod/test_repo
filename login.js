93613a5c5b82dd94430364057ad2851272c51644
Harsh Palod <156534599+harhspalod@users.noreply.github.com>
2026-04-09 07:30:24 +0530

    fix: auto-fix security issues in login.js [autopm]

--- login.js
+++ login.js
@@ -1,7 +1,26 @@
 function login(username, password) {
-  const query = "SELECT * FROM users WHERE username = '" + username + "' AND password = '" + password + "'";
-  const user = db.query(query);
+  const query = "SELECT * FROM users WHERE username = $1 AND password = $2";
+  const values = [username, password];
+  const user = db.query(query, values);
   document.getElementById('output').innerHTML = user.name;
-  eval(user.permissions);
+  const permissions = JSON.parse(user.permissions);
+  applyPermissions(permissions);
   return user;
+}
+
+function applyPermissions(permissions) {
+  const permissionMap = {
+    'admin': () => {
+      // admin permissions
+    },
+    'moderator': () => {
+      // moderator permissions
+    },
+    'user': () => {
+      // user permissions
+    }
+  };
+  if (permissions in permissionMap) {
+    permissionMap[permissions]();
+  }
 }
