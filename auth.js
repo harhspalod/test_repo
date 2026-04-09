476bba3579bbdba97607d18ffea939c3acc3781d
Harsh Palod <harshspalod@gmail.com>
2026-04-09 07:03:32 +0530

    Add auth and payment modules

---
+++ auth.js
@@ -0,0 +1,29 @@
+const db = require('./db');
+
+function login(username, password) {
+  // SQL injection vulnerability
+  const query = "SELECT * FROM users WHERE username = '" + username + "' AND password = '" + password + "'";
+  const user = db.query(query);
+  
+  // XSS vulnerability  
+  document.getElementById('welcome').innerHTML = "Welcome " + user.name;
+  
+  // Hardcoded secret
+  const API_KEY = "sk-prod-1234567890abcdef";
+  
+  // eval vulnerability
+  eval(user.permissions);
+  
+  return user;
+}
+
+function resetPassword(email) {
+  // No input validation
+  const query = "UPDATE users SET password = '" + Math.random() + "' WHERE email = '" + email + "'";
+  db.query(query);
+  
+  // Sending password in plain text email
+  sendEmail(email, "Your new password is: " + Math.random());
+}
+
+module.exports = { login, resetPassword };
